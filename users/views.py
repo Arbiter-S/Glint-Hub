c165f7277aca0f013528abd99d3227d3b6ff1154
Ali <alisarbakhshi08@gmail.com>
2024-04-25 20:16:19 +0330

    feat: User registeration and login

---
+++ users/views.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import status
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from .serializers import UserRegisterSerializer
+from .models import User
+
+
+# Create your views here.
+
+class UserRegisterView(CreateAPIView):
+    serializer_class = UserRegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        serializer = self.serializer_class(data=data)
+        serializer.is_valid(raise_exception=True)
+
+        # user creation
+        username = serializer.data['username']
+        password = serializer.data['password']
+        validate_password(password=password)  # TODO: Find a way to make the errors specefic. Right now it's just 500
+        user_model: User = get_user_model()
+        user_model.objects.create_user(username=username, password=password)
+
+        headers = self.get_success_headers(serializer.data)
+        body = {'username': username, 'message': 'User created successfully'}
+        return Response(data=body, status=status.HTTP_201_CREATED, headers=headers)
