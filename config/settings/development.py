153154f75077cb343bafcab99842aebb1bafaa20
Ali <Alisarbakhshi08@gmail.com>
2024-11-26 04:07:04 +0330

    feat: Added testing configurations and wrote tests for user registration

--- config/settings/development.py
+++ config/settings/development.py
@@ -5,3 +5,14 @@ DEBUG = True
 
 SECRET_KEY = 'django-insecure-horu%2^i!qty2t+y2vf6fhk$6*t4%6z-23qgx1p_jgevbm^df4'
 
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'TEST' : {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'test_sqlite3',
+    }
+}
+
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+import pytest
+from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient
+from django.urls import reverse
 
-# Create your tests here.
+User = get_user_model()
+
+
+@pytest.mark.parametrize('password', ['asdfghjk', 'prj8', 951983354])
+@pytest.mark.django_db
+def test_register_with_weak_password(password):
+    client = APIClient()
+    body = {
+        'username': 'SomeRandomName',
+        'password': password,
+        'email': 'testtest@gmail.com',
+    }
+    errors = {
+        'asdfghjk': 'This password is too common.',
+        'prj8': 'This password is too short. It must contain at least 8 characters.',
+        951983354: 'This password is entirely numeric.',
+    }
+
+    response = client.post(reverse('RegisterView'), body, format='json')
+    assert response.status_code == 401
+    assert response.json().get('errors')[0] == errors[password]
+@pytest.mark.django_db
+def test_successful_registration():
+    client = APIClient()
+    body = {
+        'username': 'JohnDoe',
+        'password': 'StrongPassword123!',
+        'email': 'johndoe@example.com',
+    }
+    response = client.post(reverse('RegisterView'), body, format='json')
+    assert response.status_code == 201
+    assert response.json() == {
+    "username": "JohnDoe",
+    "detail": "User created successfully"
+    }
+    qs = User.objects.filter(email='johndoe@example.com')
+    assert qs.exists()
